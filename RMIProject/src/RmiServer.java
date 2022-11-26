c6fbac91b3caf6b139fb96cf7b5742485410dd28
José Rafael <81492148+JosRafael@users.noreply.github.com>
2022-11-26 10:47:40 -1200

    Add files via upload

---
+++ RMIProject/src/RmiServer.java
@@ -0,0 +1,21 @@
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
+public class RmiServer {
+
+	public static void main(String[] args) {
+		try {
+			ICalculadora calc = new Calculadora();
+			String objNome = "rmi://localhost/calc";
+			System.out.println("Registrando o objeto no RMIRegistry...");
+			LocateRegistry.createRegistry(1099);
+			Naming.rebind(objNome, calc);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
